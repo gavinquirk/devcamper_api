da2629330b57244c3cc100b82bdb6965d424f930
Gavin Quirk <gavinquirk@gmail.com>
2019-11-18 12:48:28 -0800

    rate limiting, param pollution protection, cors enabled

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -48,6 +51,20 @@ app.use(helmet());
 
 // Prevent XSS Attacks
 app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent HTTP Param Pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
